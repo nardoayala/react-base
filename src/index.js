b4d885d74444853f869ae75f29e2a86039217ef1
Bernardo Ayala <ba@bernardoayala.com>
2021-04-18 08:52:07 -0400

    All dependencies updated with ncu

--- src/index.js
+++ src/index.js
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 import App from './containers/App';
 import './styles/main.scss';
 
-ReactDOM.render(<App />, document.getElementById('app'));
+const app = React.createElement(App);
+ReactDOM.render(app, document.getElementById('app'));
